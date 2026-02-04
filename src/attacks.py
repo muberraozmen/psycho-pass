256ceaa77edb9c173ae6f8974475e57b817435d3
Muberra Ozmen <36414569+muberraozmen@users.noreply.github.com>
2026-02-04 00:16:55 -0500

    changed the true/false description

--- src/attacks.py
+++ src/attacks.py
@@ -1,4 +1,5 @@
 import os
+from pyrit.models import Seed
 from pyrit.prompt_target import OpenAIChatTarget
 from pyrit.executor.attack import (
     AttackAdversarialConfig,
@@ -8,6 +9,21 @@ from pyrit.executor.attack import (
 )
 from pyrit.executor.attack import AttackScoringConfig
 from pyrit.score import SelfAskTrueFalseScorer, TrueFalseQuestionPaths
+
+from typing import Union
+
+
+__all__ = ["run_attack", "build_red_teaming_attack"]
+
+
+async def run_attack(attack: Union[RedTeamingAttack], seed: Seed) -> None:
+    return await attack.execute_async(
+        objective=seed.value, 
+        memory_labels={
+            "data_type": seed.data_type if seed.data_type else "text",
+            "harm_categories": seed.harm_categories if seed.harm_categories else None,
+        }
+    )
 
 
 def build_chat_model(cfg: dict) -> OpenAIChatTarget:    
@@ -43,7 +59,7 @@ def build_chat_model(cfg: dict) -> OpenAIChatTarget:
         raise ValueError(f"Unsupported model type: {cfg.get('type')}")
 
 
-def RTA(cfg: dict) -> RedTeamingAttack:    
+def build_red_teaming_attack(cfg: dict) -> RedTeamingAttack:    
     """
     Make a red teaming attack configuration.
     """
@@ -64,11 +80,13 @@ def RTA(cfg: dict) -> RedTeamingAttack:
     scoring_config = AttackScoringConfig(
         objective_scorer=SelfAskTrueFalseScorer(
             chat_target=scoring_model,
-            true_false_question_path=TrueFalseQuestionPaths.QUESTION_ANSWERING.value
+            # NOTE: This will come in next release of PyRIT.
+            # true_false_question_path=TrueFalseQuestionPaths.TASK_ACHIEVED.value 
+            true_false_question_path="src/task_achieved.yaml"
         ),
     )
     '''
-    NOTE: source of TrueFalseQuestionPaths.QUESTION_ANSWERING.value
+    NOTE: common source of TrueFalseQuestionPaths.QUESTION_ANSWERING.value
     https://github.com/Azure/PyRIT/blob/main/pyrit/datasets/score/true_false_question/question_answering.yaml
     Alternative: TrueFalseQuestionPaths.TASK_ACHIEVED.value
     https://github.com/Azure/PyRIT/blob/main/pyrit/datasets/score/true_false_question/task_achieved.yaml
@@ -79,5 +97,4 @@ def RTA(cfg: dict) -> RedTeamingAttack:
         attack_adversarial_config=adversarial_config,
         attack_scoring_config=scoring_config,
         max_turns=cfg.get("max_turns", 3))
-
 
